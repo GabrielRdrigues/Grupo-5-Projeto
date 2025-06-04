fa5d0e4eea9fd9b78e926875390d55c7562e9fdc
Matheus Oliveira Monteiro <mathmonteiro86@gmail.com>
2025-06-04 00:15:52 -0300

    feature: Adicionando redirecionamento da view product para a main

--- projeto focus/js/xpGeek.js
+++ projeto focus/js/xpGeek.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", function ()
     const botoes = document.querySelectorAll("#botoes button"); // Pega todos os botoes (filtros da tela)
     const elementos = document.querySelectorAll(".elemento"); // Pega todos os elementos (produtos)
 
-    //Adiciona a classe ativo ao INICIO quando inicia.
-    botoes[0].classList.add("ativo");
+    //Verifica se a pagina atual é a de produtos ou a main
+    const paginaproduto = document.body.dataset.page === "viewprodutos";
+
+    if(!paginaproduto){
+        //Adiciona a classe ativo ao INICIO quando inicia.
+        botoes[0].classList.add("ativo");
+    }
 
     botoes.forEach(function (botao) 
     {
@@ -21,33 +26,58 @@ document.addEventListener("DOMContentLoaded", function ()
             botao.classList.add("ativo");
 
             //Fim estilizacao
-
-            elementos.forEach(function (elemento) 
-            {
-                if (filtro === "Inicio") 
-                {
-                    elemento.style.display = "flex";
-                } 
-                else 
-                {
-                    const tipo = elemento.getAttribute("tipo");
-                    const filtroFormatado = filtro.replace(/\s/g, '');
-                    /* 
-                        \s remove espaço, tab
-                        o /g diz para fazer isso múltiplas vezes
-                        estamos dando "replace" nos espaços pelo caracter ''
-                        Isto aqui só serve pra tirar o espaço do "Action Figures"
-                    */
-                    elemento.style.display = tipo === filtroFormatado ? "flex" : "none";
-                    /*
-                    Se a variável "tipo" do elemento for igual ao botão clicado (representado pela variável
-                    filtroFormatado), iremos mostrar o elemento, isto é, seu style.display recebe "flex"; se não,
-                    recebe "none" e o elemento desaparece.
-                    */
-                }
-            });
+            
+            //Verifica se esta na pagina de vizualizao de produto e troca de pagina, indo para a pagina clicada
+            if(paginaproduto){
+                //armazena o filtro clicado
+                localStorage.setItem("filtroSelecionado",filtro);
+                window.location.href = `xpGeek.html`;
+            }
+            else{
+                aplicarFiltro(filtro);
+            }
         });
-    });
+    });       
+    
+    //Carrega o Filtro armazenado
+    const filtroSalvo = localStorage.getItem("filtroSelecionado");
+    if(!paginaproduto && filtroSalvo){
+        //Encontra botao que texto === filtroSalvo e realiza o clique
+        const botaoFiltrado = Array.from(botoes).find(btn => btn.textContent === filtroSalvo);
+        if(botaoFiltrado){
+            botaoFiltrado.click();
+        }
+        localStorage.removeItem("filtroSelecionado");
+    }
+    
+    //Refatoracao dos filtros do gabriel para uma funcao para que seja possivel reutilizar
+    function aplicarFiltro(filtro){
+        elementos.forEach(function (elemento) 
+        {
+            if (filtro === "Inicio") 
+            {
+                elemento.style.display = "flex";
+            } 
+            else 
+            {
+                const tipo = elemento.getAttribute("tipo");
+                const filtroFormatado = filtro.replace(/\s/g, '');
+                /* 
+                    \s remove espaço, tab
+                    o /g diz para fazer isso múltiplas vezes
+                    estamos dando "replace" nos espaços pelo caracter ''
+                    Isto aqui só serve pra tirar o espaço do "Action Figures"
+                */
+                elemento.style.display = tipo === filtroFormatado ? "flex" : "none";
+                /*
+                Se a variável "tipo" do elemento for igual ao botão clicado (representado pela variável
+                filtroFormatado), iremos mostrar o elemento, isto é, seu style.display recebe "flex"; se não,
+                recebe "none" e o elemento desaparece.
+                */
+            }
+        });            
+    }        
+        
 });
 
 var typed = new Typed(".auto-type", {
